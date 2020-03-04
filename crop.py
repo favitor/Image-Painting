650f5b9a897c5c703ed690f4b0c8025a720b2e15
vitorfa <42816132+favitor@users.noreply.github.com>
2020-03-03 21:29:40 -0300

    Update crop.py
    testing

--- crop.py
+++ crop.py
@@ -5,9 +5,10 @@ cropping = False
  
 x_start, y_start, x_end, y_end = 0, 0, 0, 0
  
-image = cv2.imread('test.jpg')
+image = cv2.imread('base.jpg')
+height, width = image.shape[:2]
 oriImage = image.copy()
- 
+#img = np.zeros((512,512,3), np.uint8)
  
 def mouse_crop(event, x, y, flags, param):
     # grab references to the global variables
@@ -35,14 +36,15 @@ def mouse_crop(event, x, y, flags, param):
         if len(refPoint) == 2: #when two points were found
             roi = oriImage[refPoint[0][1]:refPoint[1][1], refPoint[0][0]:refPoint[1][0]]
             cv2.imshow("Cropped", roi)
+            dsize = height, width
+            resized_roi = cv2.resize(roi, dsize)
+            mask  = cv2.imwrite('mask.jpg', resized_roi)
  
 cv2.namedWindow("image")
 cv2.setMouseCallback("image", mouse_crop)
  
 while True:
- 
     i = image.copy()
- 
     if not cropping:
         cv2.imshow("image", image)
  
@@ -50,6 +52,6 @@ while True:
         cv2.rectangle(i, (x_start, y_start), (x_end, y_end), (255, 0, 0), 2)
         cv2.imshow("image", i)
  
-    cv2.waitKey(1)
- 
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break    
 cv2.destroyAllWindows()
